2719a80b94d633fa0d7c6b01b3adab94f2358152
Pavel Chroust <pavel1chroust@gmail.com>
2025-09-22 18:57:18 +0200

    vercel config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: './app',  
+export default defineConfig({  
   plugins: [react()],
 })
