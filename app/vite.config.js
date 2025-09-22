e19b0dcfcf662d8927a8f8a960de54c396fc6de5
Pavel Chroust <pavel1chroust@gmail.com>
2025-09-22 18:35:32 +0200

    deployment config

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PortfolioWebsite/',  // <-- replace this with your GitHub repo name
+  base: './app',  
   plugins: [react()],
 })
